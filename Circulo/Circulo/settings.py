a87dbfcce1e5ad65d5432371974131c851fcd683
NachoVidondo <ignaciovidondo@hotmail.com>
2021-03-23 01:32:25 -0300

    static

--- Circulo/Circulo/settings.py
+++ Circulo/Circulo/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/vps-1944930-x.dattaweb.com/static/"
+STATIC_ROOT = "/var/www/html/static/"
 
 
 
